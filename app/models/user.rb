e05abc25c57d83a2c835b3a71d99e80a411a363c
Stefan <stefan.smagula@gmail.com>
2012-03-03 16:46:57 -0500

    Trying to fix heroku db problem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
     has_many :microposts
+    validates_presence_of :name, :on => :create, :message => "Everyone needs a name!"
+    validates_presence_of :email, :on => :create, :message => "Please enter an email address"
+    validates_uniqueness_of :name, :on => :create, :message => "already being used. Please choose another."
+    validates_uniqueness_of :email, :on => :create, :message => "already being used. Please choose another."
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Which::Application.routes.draw do
   resources :microposts
 
   resources :users
-
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
